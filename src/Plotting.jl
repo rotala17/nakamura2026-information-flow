2ea61fab6b9dbe9af9d4e1912f68d905de8e6c50
rotala17 <rotala.bio@gmail.com>
2025-10-07 14:35:12 +0900

    Cell ID modified, bootstrap bias and distribution is visualized

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -6,24 +6,29 @@ function plot_MI(MI;only_legend=false)
     colors = [blue,red]
     markershapes = [:dtriangle,:utriangle]
     label_uplw = ["upper bound: ","lower bound: "]
-    label_mutant = ["RB+","RB-"]
+    label_mutant = ["RB+","RB\$-\$"]
     w = 0.1
     offset = [w,-w]
+    offset_mutant = [0,n_cells + 0.5]
+
     plot()
     for i_mutant in 1:n_mutants, i_cell in 1:n_cells
-        cell_ID = (i_mutant-1)*n_cells + i_cell
+        #cell_ID = (i_mutant-1)*n_cells + i_cell
         for i_uplw in 1:2
             label = (i_cell == 1) ? label_uplw[i_uplw]*label_mutant[i_mutant] : ""
-            scatter!([cell_ID + offset[i_uplw]],[mean(MI[i_uplw][:,i_mutant,i_cell])],
+            scatter!([i_cell + offset[i_uplw] + offset_mutant[i_mutant]],[mean(MI[i_uplw][:,i_mutant,i_cell])],
                     yerror = std(MI[i_uplw][:,i_mutant,i_cell]),
                     color = colors[i_mutant], markerstrokecolor=colors[i_mutant],
                     markershape = markershapes[i_uplw],label=label)
         end
     end
+    
+
     if only_legend
         plot!(xlim=(-1,-1),ticks=false,showaxis=false,legend=:left)
     else
-        plot!(xlim=(1-5w,n_mutants*n_cells+5w), ylim=(-0.25,1.),yticks = -0.2:0.2:1.0,xlabel = "Cell ID",ylabel="\$\\mathcal{I}[\\mathcal{X}_{t};Y_{t}]\\ (\\mathrm{bit})\$",legend=false,yguidefontfamily="Bookman")
+        plot!(xlim=(1-5w,n_mutants*n_cells+1+5w), ylim=(-0.25,1.),yticks = -0.2:0.2:1.0,xlabel = "Cell ID",ylabel="\$\\mathcal{I}[\\mathcal{X}_{t};Y_{t}]\\ (\\mathrm{bit})\$",legend=false,yguidefontfamily="Bookman")
+        plot!(xticks = (reshape([i_cell + offset_mutant[i_mutant] for i_cell in 1:n_cells,i_mutant in 1:n_mutants],n_cells*n_mutants),["$(label_mutant[i_mutant])$(i_cell)" for i_cell in 1:n_cells,i_mutant in 1:n_mutants]),xrotation=45)
     end
 end
 
